5a2293324af88fd29461b8a7d774b4244947f0ec
shahar muky <shahar.muky@gmail.com>
2025-12-05 23:51:55 +0200

    moving getAllTeams to be lazy

--- src/main/java/com/muky/toto/config/IsraelLeagueConfig.java
+++ src/main/java/com/muky/toto/config/IsraelLeagueConfig.java
@@ -1,6 +1,8 @@
 package com.muky.toto.config;
 
 import com.muky.toto.model.IsraelLeagueType;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,27 +10,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Setter
+@Getter
 @Configuration
 @ConfigurationProperties(prefix = "app.league.israel")
 public class IsraelLeagueConfig {
     private Integer seasonId;
     private List<LeagueConfig> leagues;
-
-    public Integer getSeasonId() {
-        return seasonId;
-    }
-
-    public void setSeasonId(Integer seasonId) {
-        this.seasonId = seasonId;
-    }
-
-    public List<LeagueConfig> getLeagues() {
-        return leagues;
-    }
-
-    public void setLeagues(List<LeagueConfig> leagues) {
-        this.leagues = leagues;
-    }
 
     /**
      * Get league configuration by type
--- src/main/java/com/muky/toto/config/LeagueConfig.java
+++ src/main/java/com/muky/toto/config/LeagueConfig.java
@@ -1,44 +1,17 @@
 package com.muky.toto.config;
 
 import com.muky.toto.model.IsraelLeagueType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
+@Getter
+@Setter
 public class LeagueConfig {
     private IsraelLeagueType type;
     private String name;
     private Integer leagueId;
     private Map<String, String> team;
-
-    public IsraelLeagueType getType() {
-        return type;
-    }
-
-    public void setType(IsraelLeagueType type) {
-        this.type = type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getLeagueId() {
-        return leagueId;
-    }
-
-    public void setLeagueId(Integer leagueId) {
-        this.leagueId = leagueId;
-    }
-
-    public Map<String, String> getTeam() {
-        return team;
-    }
-
-    public void setTeam(Map<String, String> team) {
-        this.team = team;
-    }
 }
+
