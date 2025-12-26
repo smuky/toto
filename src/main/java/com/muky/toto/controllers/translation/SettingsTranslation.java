f44065a5306c587cd60e6dde61f3d15ff7503168
shahar muky <shahar.muky@gmail.com>
2025-12-26 21:36:50 +0200

    add SettingsTranslation record to manage translation strings for settings

---
+++ src/main/java/com/muky/toto/controllers/translation/SettingsTranslation.java
@@ -0,0 +1,15 @@
+package com.muky.toto.controllers.translation;
+
+public record SettingsTranslation(
+        String settings,
+        String general,
+        String language,
+        String support,
+        String sendFeedback,
+        String sendFeedbackSubtitle,
+        String manageSubscription,
+        String information,
+        String about,
+        String termsOfUsePrivacyPolicy
+) {
+}
